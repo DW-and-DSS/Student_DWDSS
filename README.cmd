55607e02b31b147c815cae6cdf72988d8e9cb636
Lê Nguyễn Yến Nhi <142215196+LeNguyenYenNhi2812@users.noreply.github.com>
2025-04-24 11:01:42 +0700

    Create README.cmd

---
+++ README.cmd
@@ -0,0 +1,59 @@
+# Student Score Analysis
+
+## Project Title
+
+**DW Assignment - Student Score Analysis**
+
+## Description
+
+This project focuses on analyzing various factors that influence students' academic performance using a data warehouse and decision support system approach. By applying data-driven techniques, the goal is to help enhance learning outcomes, improve teaching methodologies, and support resource allocation decisions in educational settings.
+
+### Key Benefits
+- Data-driven decision-making  
+- Improved student success rates  
+- Efficient resource allocation  
+- Enhanced student engagement  
+- Real-world applicability in early intervention and personalized learning  
+
+## Getting Started
+
+### Dependencies
+
+* None explicitly required
+
+### Installing
+
+This project is part of a course assignment and does not require an installation process.  
+
+### Executing Program
+
+The workflow includes the following phases:
+
+#### 1. Data Understanding
+- Analyze features such as study time, sleep, attendance, physical activity, gender, and school type...
+
+#### 2. ETL Process
+- **Clean data**: Handle missing values (remove rows or fill based on data type)
+- **Transform data**: Average for numeric, mode/lowercase for strings
+- **Load into database**: Structured loading for analysis
+
+#### 3. Visualization
+- **Study Time**: Slight positive correlation with exam scores, especially after 18 hours
+- **Attendance**: Slightly improves performance
+- **Sleep**: Better sleep improves scores
+- **Physical Activity**: No strong correlation
+- **Gender**: No major difference
+- **School Type**: Private school students tend to score slightly higher
+> To explore our interactive data visualizations and dashboards, visit our Power BI report:  
+👉 [Click here to view the Power BI report](https://app.powerbi.com/links/eHBm55DwFE?ctid=7bbbced8-b31a-4a36-95bb-9f06bc9d72a6&pbi_source=linkShare)
+
+#### 4. Insights & Recommendations
+- Gradually increase and segment study time
+- Promote healthy sleep habits
+- Encourage class attendance and engagement
+- Improve resources in public schools
+and so on...
+
+
+
+### Common Issues
